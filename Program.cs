ddeed9d0a52f7f121f99a647fcbd90408728d77c
Bárbara <babyr.oliveira@gmail.com>
2020-06-01 17:12:41 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IdadeAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Pressione enter para saber mais informações sobre mim (Bárbara):");
+            Console.ReadKey();
+
+            DateTime barbara = new DateTime (2004, 04, 06, 16, 13, 00);
+            string barbaradtl = barbara.ToLongDateString(); //dtl = data longa 
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"Eu nasci em {barbaradtl}");
+
+            DateTime bday = barbara.AddYears(18);
+            string bday18 = bday.ToLongDateString();
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"Meu aniversário de 18 anos é {bday18}");
+        }
+    }
+}
